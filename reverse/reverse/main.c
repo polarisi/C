8926f9f1116fa17d94f36dc101d6b2b26f60559e
2717424952 <2717424952@qq.com>
2023-09-05 21:20:45 +0800

    翻转eg:I like you.   --->you. like I

---
+++ reverse/reverse/main.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+void reverse(char* left, char* right)
+{
+	while (left < right)
+	{
+		char tmp = *left;
+		*left = *right;
+		*right = tmp;
+		left++;
+		right--;
+	}
+}
+int main()
+{
+	char arr[100] = { 0 };
+	gets(arr);
+	int len = strlen(arr);
+	reverse(arr, arr + len - 1);
+	char* start = arr;
+	while (*start)
+	{
+		char* end = start;
+		while (*end != ' ' && *end != '\0')
+		{
+			end++;
+		}
+		reverse(start, end - 1);
+		if(*end != '\0')
+		end++;
+		start = end;
+	}
+	printf("%s\n", arr);
+	return 0;
+}
