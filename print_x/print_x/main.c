3173cff452572a36296b4267c8ee253ad1ba13e0
2717424952 <2717424952@qq.com>
2023-09-06 19:03:02 +0800

    打印*和空格组成的X

---
+++ print_x/print_x/main.c
@@ -0,0 +1,25 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+int main()
+{
+	int n;
+	while ((scanf("%d", &n)) == 1)
+	{
+		int i = 0;
+		int j = 0;
+		for (i = 0; i < n; i++)
+		{
+			for (j = 0; j < n; j++)
+			{
+				if (i == j)
+					printf("*");
+				else if (i + j == n - 1)
+					printf("*");
+				else
+					printf(" ");
+			}
+			printf("\n");
+		}
+	}
+	return 0;
+}
